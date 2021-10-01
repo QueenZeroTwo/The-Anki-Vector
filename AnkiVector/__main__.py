3bffb91c1997c5a465d58f159e9102acf94bea1b
φµεεɳZero†wo <91667256+QueenZeroTwo@users.noreply.github.com>
2021-10-01 11:07:27 +0530

    Update __main__.py

--- AnkiVector/__main__.py
+++ AnkiVector/__main__.py
@@ -362,7 +362,7 @@ def help_button(update, context):
 
 
 @run_async
-def ZeroTwo_about_callback(update, context):
+def  AnkiVector_about_callback(update, context):
     query = update.callback_query
     if query.data == "aboutmanu_":
         query.message.edit_text(
